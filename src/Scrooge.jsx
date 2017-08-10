f222d6cf1a9a330f9b099db5d089fd018d44a69e
Gleb Kostyunin <gleb.kostyunin@sinnerschrader.com>
2017-08-11 00:13:14 +0200

    Add glamorous and try basic styling

--- src/Scrooge.jsx
+++ src/Scrooge.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { fetchCurrencies } from './actions';
+import glamorous from 'glamorous'
+import CurrencyCard from './components/currency-card.jsx';
+
+
+const Container = glamorous.div({
+  maxWidth: '700px',
+  margin: '0 auto'
+});
 
 class Scrooge extends React.Component {
   constructor(props) {
@@ -14,14 +22,14 @@ class Scrooge extends React.Component {
 
   render() {
     const { currencies, error, isFetching } = this.props;
-    if (isFetching) return (<div>Fetching data... </div>);
-    return (<div>
+    if (isFetching) return (<Container textAlign="center">Fetching data... </Container>);
+    return (<Container>
         {
           error
           ? <div className="error">{error}</div>
-          : currencies && currencies.map(cur => <div>{cur.name}</div>)
+          : currencies && currencies.map(cur => <CurrencyCard currency={cur} />)
         }
-      </div>);
+      </Container>);
   }
 }
 
---
+++ src/components/currency-card.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import glamorous, {Div} from 'glamorous';
+
+const cardBG = '#FFFDDF';
+
+const CardContainer = glamorous.div({
+  padding: '20px 45px',
+  background: cardBG,
+  background: `linear-gradient(135deg, transparent 20px, ${cardBG} 0)`,
+  borderRadius: '5px '
+});
+
+const CardHeading = glamorous.h1({
+  marginBottom: '5px',
+  fontSize: '36px',
+  fontWeight: 'normal'
+});
+
+
+class CurrencyCard extends React.Component {
+  render() {
+    const {
+      name,
+      price_eur,
+      percent_change_1h,
+      percent_change_24h,
+      percent_change_7d
+    } = this.props.currency;
+    return (
+      <Div marginBottom='45px'>
+        <CardHeading>{name}</CardHeading>
+        <CardContainer>
+          <Div display="flex" textAlign="center" justifyContent="space-between">
+            <div>
+              <p>Value</p>
+              <p>{parseFloat(price_eur).toFixed(3) + ' EUR'}</p>
+            </div>
+            <Div display="flex" flexBasis="60%" justifyContent="space-around">
+              <div>
+                <p>1h</p>
+                <p>{percent_change_1h}</p>
+              </div>
+              <div>
+                <p>24h</p>
+                <p>{percent_change_24h}</p>
+              </div>
+              <div>
+                <p>7d</p>
+                <p>{percent_change_7d}</p>
+              </div>
+            </Div>
+          </Div>
+        </CardContainer>
+      </Div>
+    );
+  }
+}
+
+export default CurrencyCard;
