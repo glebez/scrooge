df4a1a9e9c05d70a0f6afe359a00231fb5fa9d86
Gleb Kostyunin <gleb.kostyunin@sinnerschrader.com>
2017-08-17 21:53:58 +0200

    add redux devtools

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { middleware as reduxPackMiddleware } from 'redux-pack';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import Scrooge from './Scrooge.jsx';
 import scroogeApp from './reducers';
 import './styles/globals';
 
-const store = createStore(scroogeApp, applyMiddleware(reduxPackMiddleware));
+const store = createStore(scroogeApp, composeWithDevTools(applyMiddleware(reduxPackMiddleware)));
 
 ReactDOM.render(
   <Provider store={store}>
