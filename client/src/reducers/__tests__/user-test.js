80b49f03f7c7b58510f61601e56d20829be87950
Gleb Kostyunin <gleb.kostyunin@sinnerschrader.com>
2017-09-17 22:33:06 +0200

    setup unit tests with mocha and chai

---
+++ client/src/reducers/__tests__/user-test.js
@@ -0,0 +1,9 @@
+import { assert } from 'chai';
+import reducer from '../user.js';
+
+describe('user reducer', () => {
+  // @todo: add actual tests
+  it('should work', () => {
+    assert.isTrue(true);
+  });
+});
---
+++ server/src/controllers/__tests__/userController-test.js
@@ -0,0 +1,8 @@
+import { assert } from 'chai';
+
+describe('user controller', () => {
+  // @todo: add actual tests
+  it('should work', () => {
+    assert.isTrue(true);
+  });
+});
