15862daf5db5eb8a256fb101ea4615148876bca4
Gleb Kost <glebkost.dev@gmail.com>
2017-11-12 18:42:41 +0100

    configure deployment to heroku

--- client/webpack.client.config.js
+++ client/webpack.client.config.js
@@ -1,6 +1,54 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+
+const plugins = [
+  new CleanWebpackPlugin(['dist']),
+  new HtmlWebpackPlugin({
+    template: './client/src/index.html',
+  }),
+  new webpack.EnvironmentPlugin(['NODE_ENV']),
+];
+
+let rules = [
+  {
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    loader: 'babel-loader'
+  },
+  {
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    loader: ['babel-loader', 'eslint-loader']
+  },
+  {
+    test: /\.(png|svg|jpg|gif|ico)$/,
+    loader: 'file-loader',
+    options: {
+      name: '[name].[ext]'
+    }
+  }
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(new UglifyJSPlugin());
+  rules = [
+    {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    },
+    {
+      test: /\.(png|svg|jpg|gif|ico)$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]'
+      }
+    }
+  ];
+}
 
 module.exports = {
   entry: './client/src/index.js',
@@ -9,36 +57,13 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     publicPath: '/static/',
   },
-  plugins: [
-    new CleanWebpackPlugin(['dist']),
-    new HtmlWebpackPlugin({
-      template: './client/src/index.html'
-    })
-  ],
+  plugins,
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist'
   },
   module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: ['babel-loader', 'eslint-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|gif|ico)$/,
-        loader: 'file-loader',
-        options: {
-          name: '[name].[ext]'
-        }
-      }
-    ]
+    rules,
   },
-  resolve: {extensions: ['.js', '.jsx']}
+  resolve: { extensions: ['.js', '.jsx'] }
 };
--- server/webpack.server.config.js
+++ server/webpack.server.config.js
@@ -1,5 +1,52 @@
+const webpack = require('webpack');
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const plugins = [
+  new CleanWebpackPlugin(['dist']),
+  new webpack.EnvironmentPlugin(['NODE_ENV']),
+];
+
+let rules = [
+  {
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    loader: 'babel-loader'
+  },
+  {
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    loader: ['babel-loader', 'eslint-loader']
+  },
+  {
+    test: /\.(png|svg|jpg|gif|ico)$/,
+    loader: 'file-loader',
+    options: {
+      emitFile: false,
+      publicPath: '/static/',
+      name: '[name].[ext]'
+    }
+  }
+];
+
+if (process.env.NODE_ENV === 'production') {
+  rules = [
+    {
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    },
+    {
+      test: /\.(png|svg|jpg|gif|ico)$/,
+      loader: 'file-loader',
+      options: {
+        emitFile: false,
+        publicPath: '/static/',
+        name: '[name].[ext]'
+      }
+    }
+  ]
+}
 
 module.exports = {
   target: 'node',
@@ -8,32 +55,10 @@ module.exports = {
     filename: 'server.js',
     path: path.resolve(__dirname, 'dist')
   },
-  plugins: [
-    new CleanWebpackPlugin(['dist'])
-  ],
+  plugins,
   devtool: 'inline-source-map',
   module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: ['babel-loader', 'eslint-loader']
-      },
-      {
-        test: /\.(png|svg|jpg|gif|ico)$/,
-        loader: 'file-loader',
-        options: {
-          emitFile: false,
-          publicPath: '/static/',
-          name: '[name].[ext]'
-        }
-      }
-    ]
+    rules,
   },
-  resolve: {extensions: ['.js', '.jsx']}
+  resolve: { extensions: ['.js', '.jsx'] }
 };
