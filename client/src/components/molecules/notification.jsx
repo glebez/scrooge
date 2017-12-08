84543bd86e7a4d3d86118c37afaf4ca6ca03a945
Gleb Kost <glebkost.dev@gmail.com>
2017-12-08 13:35:55 +0100

    add Notification component

---
+++ client/src/components/molecules/notification.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import glamorous from 'glamorous';
+import { colors } from '../../styles/variables';
+
+const NotificationBody = glamorous.div({
+  position: 'relative',
+  borderRadius: '5px',
+  padding: '20px 25px',
+  color: colors.paperYellow,
+},
+({ type }) => ({
+  backgroundColor: type === 'error' ? colors.ketchupRed : colors.moscowSky,
+}),
+);
+
+const NotificationCloseBtn = glamorous.button({
+  position: 'absolute',
+  top: '5px',
+  right: '10px',
+  fontSize: '20px',
+  color: 'inherit',
+  background: 'none',
+  border: 'none',
+});
+
+function Notification({ children, onDismiss, type }) {
+  return (<NotificationBody type={type}>
+    <NotificationCloseBtn onClick={onDismiss}>&times;</NotificationCloseBtn>
+    {children}
+  </NotificationBody>);
+}
+
+Notification.propTypes = {
+  children: PropTypes.any,
+  type: PropTypes.string,
+  onDismiss: PropTypes.func,
+};
+
+export default Notification;
